3c36c28a97ca8891277e39b166e293b2f61cd6db
derokki <93949893+derokki@users.noreply.github.com>
2023-09-19 14:49:43 +0200

    Create rtl-wslider.js

---
+++ rtl-wslider.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Check if the HTML tag has a "dir" attribute set to "rtl"
+  const dirAttribute = document.documentElement.getAttribute('dir');
+
+  if (dirAttribute === 'rtl') {
+    // Select all elements with the rtl_slider attribute or the .rtl_slider-order class
+    const sliders = document.querySelectorAll('[rf-reverse-wslider="slider"]');
+
+    // Loop through each matching element
+    sliders.forEach(slider => {
+      // Get .w-slider-mask
+      const sliderMask = slider.querySelector('.w-slider-mask');
+
+      // Get every child of .w-slide and .w-slider-mask
+      const slideChildren = slider.querySelectorAll('.w-slide > *, .w-slider-mask > *');
+
+      // Rotate .w-slider-mask and all children by 180 degrees
+      [sliderMask, ...slideChildren].forEach(child => {
+        if (child) {
+          child.style.transform = 'rotate(180deg)';
+        }
+      });
+    });
+  }
+});
