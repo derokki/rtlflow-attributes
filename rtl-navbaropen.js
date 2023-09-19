b6e79bd741be19f5ac257fb77ed99e202a97b06b
derokki <93949893+derokki@users.noreply.github.com>
2023-09-19 15:28:13 +0200

    Create rtl-navbaropen.js

---
+++ rtl-navbaropen.js
@@ -0,0 +1,39 @@
+// RTL Navmenu Open Reverse
+// Attribute (reverse on RTL): "rf-reverse-navbarmenu" = "rtl-navbar"
+// Attribute (reverse on LTR): "rf-reverse-navbarmenu" = "ltr-navbar"
+// set on main Navbar element
+
+document.addEventListener("DOMContentLoaded", function() {
+
+	// Check if the HTML tag has a "dir" attribute set to "rtl"
+	const dirAttribute = document.documentElement.getAttribute('dir');
+	if (dirAttribute === 'rtl') {
+
+		// Get all elements that either have the attribute Attribute: "rf-reverse-navbarmenu" = "navbar"
+		const elements = document.querySelectorAll('[rf-reverse-navbarmenu="ltr-navbar"]');
+
+		// Loop through each element and update attributes
+		elements.forEach(element => {
+		  const currentAnimation = element.getAttribute('data-animation');
+			// Toggle between 'over-right' and 'over-left'
+			element.setAttribute( 
+    		'data-animation', currentAnimation === 'over-right' ? 'over-left' : 'over-right'
+  	  );
+		});
+	}
+  
+  else if (dirAttribute === 'ltr') {
+
+		// Get all elements that either have the attribute Attribute: "rf-reverse-navbarmenu" = "navbar"
+		const elements = document.querySelectorAll('[rf-reverse-navbarmenu="rtl-navbar"]');
+
+		// Loop through each element and update attributes
+		elements.forEach(element => {
+		  const currentAnimation = element.getAttribute('data-animation');
+			// Toggle between 'over-right' and 'over-left'
+			element.setAttribute( 
+    		'data-animation', currentAnimation === 'over-right' ? 'over-left' : 'over-right'
+  	  );
+		});
+	}
+});
