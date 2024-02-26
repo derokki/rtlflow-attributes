e0b61bd5b91c30fbeec3de17d690d85b99171ac9
derokki <93949893+derokki@users.noreply.github.com>
2024-02-26 17:40:47 +0100

    Update rtl-wslider.js

--- rtl-wslider.js
+++ rtl-wslider.js
@@ -1,14 +1,16 @@
-// [RTLflow 2.0 Attributes by Derokki] RTL Webflow Slider
-// Attribute: "rf-reverse-wslider" = "slider"
-// Set on Main Slider Component. This Code reverses the Slider Mask direction. Make sure to develop Slider in an RTL-friendly way.
+// [RTLflow Attributes by Derokki] RTL Webflow Slider
+// Attribute: "rf-reverse-wslider" = "slider-manual" or "slider-auto"
+// Set on Main Slider Component. This code elegantly reverses the Slider Mask direction, ensuring RTL compatibility.
+// "slider-auto" intelligently sets the dir attribute on each slide, aligning with the HTML dir attribute.
+// "slider-manual" requires manual setting of the dir attribute on each slide for individual locale control.
 
 document.addEventListener("DOMContentLoaded", function() {
   // Check if the HTML tag has a "dir" attribute set to "rtl"
   const dirAttribute = document.documentElement.getAttribute('dir');
 
   if (dirAttribute === 'rtl') {
-    // Select all elements with the rtl_slider attribute or the .rtl_slider-order class
-    const sliders = document.querySelectorAll('[rf-reverse-wslider="slider"]');
+    // Select all elements with the rf-reverse-wslider attribute set to "slider-manual" or "slider-auto"
+    const sliders = document.querySelectorAll('[rf-reverse-wslider="slider-manual"], [rf-reverse-wslider="slider-auto"]');
 
     // Loop through each matching element
     sliders.forEach(slider => {
@@ -24,6 +26,15 @@ document.addEventListener("DOMContentLoaded", function() {
           child.style.transform = 'rotate(180deg)';
         }
       });
+
+      // If rf-reverse-wslider is set to "slider-auto", add dir="rtl" attribute to all children of .w-slider-mask
+      if (slider.getAttribute('rf-reverse-wslider') === 'slider-auto') {
+        slideChildren.forEach(child => {
+          if (child) {
+            child.setAttribute('dir', 'rtl');
+          }
+        });
+      }
     });
   }
 });
